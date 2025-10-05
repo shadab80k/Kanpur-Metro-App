710ba034afb2497a75527919cfff40a5265822ef
Shadab80k <mohdshadab4549@gmail.com>
2025-10-05 12:10:38 +0530

    Fix Splash Screen - Replace default with custom orange splash - Now shows solid Orange background with app icon in center - 2 second duration then smooth fade to app

--- android/app/src/main/java/com/kanpurmetro/app/MainActivity.java
+++ android/app/src/main/java/com/kanpurmetro/app/MainActivity.java
@@ -1,5 +1,12 @@
 package com.kanpurmetro.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Splash screen will auto-hide after configured duration
+    }
+}
